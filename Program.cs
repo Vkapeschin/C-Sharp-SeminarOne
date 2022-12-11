b9a2bbc05cf7b140a1dc929d1f364f78912a75eb
Vkapeschin <kapeshin-vladimir@rambler.ru>
2022-12-11 17:08:21 +0300

    Home

--- Program.cs
+++ Program.cs
@@ -6,4 +6,7 @@
 //char = '#';
 
 int a = 5, b = 3;
-Console.WriteLine("My numbers are " + a + " and " + b);
+Console.WriteLine($"My numbers are {a} and {b}");
+
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
--- Задачи/Program.cs
+++ Задачи/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿// Задача 1. Проверить, является ли первое число квадратом втого числа.
+/*
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int quad = b * b;
+
+if (a == quad)
+{
+    Console.WriteLine($"Число {a} является квадратом числа {b}");
+}
+else
+{
+    Console.WriteLine($"Число {a} не является квадратом числа {b}");
+}
+*/
+
+//Задача 2. Одна переменная. На выходе: все int от -n до n.
+
+/*
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+while (current <= n)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
